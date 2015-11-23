3b1c2bf7ca70e44990fbbae28338cb78c9b314f6
qianfeng <qianfeng@weishenmezhememan.local>
2015-11-23 14:55:42 +0800

    bucuode

---
+++ WWG3/WWG3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWG3
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 WangWeiGuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
